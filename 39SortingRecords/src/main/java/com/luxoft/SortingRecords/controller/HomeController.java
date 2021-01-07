53a6f944018453948b090d4fd7830035dfa44087
EASmirnov <ifgen.it@gmail.com>
2021-01-08 00:06:59 +0300

    Sorting records - add Edit, Delete Employee to UI

--- 39SortingRecords/src/main/java/com/luxoft/SortingRecords/controller/HomeController.java
+++ 39SortingRecords/src/main/java/com/luxoft/SortingRecords/controller/HomeController.java
@@ -1,10 +1,22 @@
 package com.luxoft.SortingRecords.controller;
 
+import com.luxoft.SortingRecords.entity.Employee;
+import com.luxoft.SortingRecords.service.EmployeeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
+
+    private EmployeeService employeeService;
+
+    public HomeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/home")
     public String home(){
@@ -13,8 +25,35 @@ public class HomeController {
     }
 
     @GetMapping("/home/employee")
-    public String employee(){
+    public String newEmployee(){
 
-        return "new-employee";
+        return "employee";
+    }
+
+    @GetMapping("/home/employee/{id}")
+    public String editEmployee(@PathVariable("id") Integer id, Model model){
+
+
+        Optional<Employee> emp = employeeService.findById(id);
+        if (emp.isPresent()){
+            model.addAttribute("employee", emp.get());
+            return "employee";
+        }
+        else {
+            return "redirect:/home";
+        }
+    }
+
+    @GetMapping("/home/employee/{id}/delete")
+    public String deleteEmployee(@PathVariable("id") Integer id, Model model){
+
+        Optional<Employee> emp = employeeService.findById(id);
+        if (emp.isPresent()){
+            model.addAttribute("employee", emp.get());
+            return "delete-employee";
+        }
+        else {
+            return "redirect:/home";
+        }
     }
 }
--- 39SortingRecords/src/main/resources/static/script.js
+++ 39SortingRecords/src/main/resources/static/script.js
@@ -17,71 +17,70 @@ $(document).ready(
         })
 
         $("#id-asc").click(
-            function (){
+            function () {
                 sortByField("id", "true");
             }
         );
         $("#firstName-asc").click(
-            function (){
+            function () {
                 sortByField("firstName", "true");
             }
         );
         $("#lastName-asc").click(
-            function (){
+            function () {
                 sortByField("lastName", "true");
             }
         );
         $("#position-asc").click(
-            function (){
+            function () {
                 sortByField("position", "true");
             }
         );
 
         $("#id-desc").click(
-            function (){
+            function () {
                 sortByField("id", "false");
             }
         );
         $("#firstName-desc").click(
-            function (){
+            function () {
                 sortByField("firstName", "false");
             }
         );
         $("#lastName-desc").click(
-            function (){
+            function () {
                 sortByField("lastName", "false");
             }
         );
         $("#position-desc").click(
-            function (){
+            function () {
                 sortByField("position", "false");
             }
         );
 
         $("#createEmployee").click(
-            function (){
+            function () {
+                $("#error-firstName").text("");
+                $("#error-lastName").text("");
+                $("#error-position").text("");
+
                 let firstName = $("#firstName").val();
                 let lastName = $("#lastName").val();
                 let position = $("#position").val();
                 let separationDate = $("#separationDate").val();
 
-                console.log("firstName: " + firstName);
-                console.log("lastName: " + lastName);
-                console.log("position: " + position);
-                console.log("separationDate: " + separationDate);
-
-                if(firstName == ""){
+                if (firstName == "") {
                     $("#error-firstName").text("Input First name");
                 }
-                if(lastName == ""){
+                if (lastName == "") {
                     $("#error-lastName").text("Input Last name");
                 }
-                if(position == ""){
+                if (position == "") {
                     $("#error-position").text("Input Position");
                 }
 
-                if (firstName != "" && lastName != "" && position != ""){
-                    if(separationDate == ""){
+                if (firstName != "" && lastName != "" && position != "") {
+                    if (separationDate == "") {
                         separationDate = null;
                     }
 
@@ -91,15 +90,15 @@ $(document).ready(
                         url: "http://localhost:8080/employee",
                         dataType: "json",
                         data: JSON.stringify({
-                            firstName : firstName,
-                            lastName : lastName,
-                            position : position,
-                            separationDate : separationDate
+                            firstName: firstName,
+                            lastName: lastName,
+                            position: position,
+                            separationDate: separationDate
                         }),
                         headers: {"Content-Type": "application/json"},
                         success: [
                             function (msg) {
-                                $("#answer").text("Employee saved with id=" + msg.id);
+                                $("#answer").text("Employee was created with id=" + msg.id);
                             }
                         ],
                         statusCode: {
@@ -109,6 +108,85 @@ $(document).ready(
                         }
                     })
                 }
+            }
+        );
+
+        $("#updateEmployee").click(
+            function () {
+                $("#error-firstName").text("");
+                $("#error-lastName").text("");
+                $("#error-position").text("");
+
+                let employeeId = $("#employeeId").val();
+                let firstName = $("#firstName-edit").val();
+                let lastName = $("#lastName-edit").val();
+                let position = $("#position-edit").val();
+                let separationDate = $("#separationDate-edit").val();
+
+                if (firstName == "") {
+                    $("#error-firstName").text("Input First name");
+                }
+                if (lastName == "") {
+                    $("#error-lastName").text("Input Last name");
+                }
+                if (position == "") {
+                    $("#error-position").text("Input Position");
+                }
+
+                if (firstName != "" && lastName != "" && position != "") {
+                    if (separationDate == "") {
+                        separationDate = null;
+                    }
+
+                    // PUT
+                    $.ajax({
+                        method: "PUT",
+                        url: "http://localhost:8080/employee/" + employeeId,
+                        dataType: "json",
+                        data: JSON.stringify({
+                            firstName: firstName,
+                            lastName: lastName,
+                            position: position,
+                            separationDate: separationDate
+                        }),
+                        headers: {"Content-Type": "application/json"},
+                        success: [
+                            function (msg) {
+                                $("#answer").text("Employee with id=" + msg.id + " was updated");
+                            }
+                        ],
+                        statusCode: {
+                            200: function () {
+                                console.log("Ok");
+                            }
+                        }
+                    })
+                }
+            }
+        );
+
+        $("#deleteEmployee").click(
+            function () {
+                let employeeId = $("#employeeId").val();
+
+                // DELETE
+                $.ajax({
+                    method: "DELETE",
+                    url: "http://localhost:8080/employee/" + employeeId,
+                    success: [
+                        function (msg) {
+                            $("#answer").text("Employee with id=" + employeeId + " was deleted");
+                        }
+                    ],
+                    statusCode: {
+                        200: function () {
+                            console.log("200");
+                        },
+                        204: function () {
+                            console.log("204");
+                        }
+                    }
+                })
             }
         );
     }
@@ -123,16 +201,18 @@ function insertIntoTable(msg) {
             "<td>" + value.lastName + "</td>" +
             "<td>" + value.position + "</td>" +
             "<td>" + value.separationDate + "</td>" +
+            "<td>" + "<a href=\"home/employee/" + value.id + "\">Edit</a>" + "</td>" +
+            "<td>" + "<a href=\"home/employee/" + value.id + "/delete\">Delete</a>" + "</td>" +
             "</tr>");
         $("#table1").append(row);
     });
 }
 
-function clearTable(){
+function clearTable() {
     $("#table1 tbody").empty();
 }
 
-function sortByField(field, asc){
+function sortByField(field, asc) {
 
     $.ajax({
         method: "GET",
